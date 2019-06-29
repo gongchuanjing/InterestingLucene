78efbc0bba5514be5a54865e2de3fb20c164bae1
gcj <longgongsanri@163.com>
2019-06-29 19:24:42 +0800

    修改创建索引保存的位置；并增加更多细节代码

--- src/tk/chuanjing/lucene/LuceneFirst.java
+++ src/tk/chuanjing/lucene/LuceneFirst.java
@@ -37,8 +37,13 @@ import org.wltea.analyzer.lucene.IKAnalyzer;
  */
 public class LuceneFirst {
 	
+	//private String pathname = "E:\\Develop\\temp\\LuceneIndex";
+	private String pathname = "index";//存放索引的位置
+	
 	/**
 	 * 创建索引
+	 * document--->分词--->倒排--->写入到索引库----介质(硬盘或者内存)
+	 * 
 	 * @throws Exception
 	 */
 	@Test
@@ -47,10 +52,10 @@ public class LuceneFirst {
 		//索引库保存到内存中，一般不用
 		//Directory directory = new RAMDirectory();
 		//保存到磁盘上
-		Directory directory = FSDirectory.open(new File("E:\\Develop\\temp\\LuceneIndex"));
+		Directory directory = FSDirectory.open(new File(pathname));
 		
 		//====2、创建一个IndexWriter对象。需要一个分析器对象。
-//		Analyzer analyzer = new StandardAnalyzer();
+		//Analyzer analyzer = new StandardAnalyzer();
 		Analyzer analyzer = new IKAnalyzer();
 		//参数1：lucene的版本号，第二个参数：分析器对象
 		IndexWriterConfig conf = new IndexWriterConfig(Version.LATEST, analyzer);
@@ -72,12 +77,17 @@ public class LuceneFirst {
 			//====4、创建文档对象
 			Document document = new Document();
 			
-			//创建域
+			/*
+			 * 创建域
+			 * 一个文档中，有很多字段，想通过name进行查询，这个字段需要被indexed。
+			 * 如果一个想创建索引，有两种选择，一种不被分词就是完全匹配，一种是分词。比如：TextField是要被分词的，而StringField是不被分词的
+			 */
 			//参数1：域的名称 参数2：域的内容 参数3：是否存储
 			TextField fileNameField = new TextField("name", fileName, Store.YES);
 			StoredField filePathField = new StoredField("path", filePath);
 			TextField fileContentField = new TextField("content", fileContent, Store.NO);
 			LongField fileSizeField = new LongField("size", fileSize, Store.YES);
+			//fileContentField.setBoost(10);//激励因子，设置权重
 			
 			//====5、向文档中添加域
 			document.add(fileNameField);
@@ -89,8 +99,30 @@ public class LuceneFirst {
 			indexWriter.addDocument(document);
 		}
 		
+		// commit将内存中等待的数据提交到硬盘创建索引。
+		// indexWriter.commit();
+		
 		//====7、关闭IndexWriter对象
 		indexWriter.close();
+		
+		/*
+		if(男的){
+			System.out.println("我今天休息");
+			
+		}else if(女的){
+			System.out.println("我现在有点忙");
+			
+		}else if(漂亮女孩){
+			System.out.println("我在316");
+			
+		}else if(田姐的学生 && 漂亮的女孩){
+			System.out.println("你在哪？我去找你");
+			
+		}else {
+			System.out.println("其实根本不需要这个else，但是被小田姐的美貌所打动，加上吧，反正不管加不加，都得帮她");
+		}
+		*/
+		
 	}
 	
 	/**
@@ -100,7 +132,7 @@ public class LuceneFirst {
 	@Test
 	public void searchIndex() throws Exception {
 		//====1指定索引库存放的位置
-		Directory directory = FSDirectory.open(new File("E:\\Develop\\temp\\LuceneIndex"));
+		Directory directory = FSDirectory.open(new File(pathname));
 		
 		//====2使用IndexReader对象打开索引库
 		IndexReader indexReader = DirectoryReader.open(directory);
@@ -111,7 +143,8 @@ public class LuceneFirst {
 		//====4创建一个查询对象,需要指定查询域及要查询的关键字。
 		//term的参数1：要搜索的域
 		//参数2：搜索的关键字
-		Query query = new TermQuery(new Term("name", "spring"));
+		//Query query = new TermQuery(new Term("name", "spring"));
+		Query query = new TermQuery(new Term("content", "全文"));
 		
 		//参数1：查询条件
 		//参数2：查询结果返回的最大值
@@ -125,6 +158,9 @@ public class LuceneFirst {
 		for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
 			//取文档id
 			int id = scoreDoc.doc;
+			System.out.println("维护在lucene内部的文档编号:"+id);
+			System.out.println("当前文档的得分:"+scoreDoc.score);
+			
 			//从索引库中取文档对象
 			Document document = indexSearcher.doc(id);
 			//取属性
